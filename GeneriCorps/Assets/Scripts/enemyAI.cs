b0a42380d711accc46fc05108152d12bb2548181
Edisonmatos5 <ejmatosespinal@student.fullsail.edu>
2025-05-12 19:58:48 -0400

    worked on some code for shooting enemy waiting on gamamanager from theo.

--- GeneriCorps/Assets/Scripts/enemyAI.cs
+++ GeneriCorps/Assets/Scripts/enemyAI.cs
@@ -2,21 +2,44 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.AI;
 
+
 public class enemyAI : MonoBehaviour
 {
     [SerializeField] Renderer model;
-    [SerializeField] int HP;
+    [SerializeField] NavMeshAgent agent;
 
+
+    [SerializeField] int HP;
+    [SerializeField] int faceTargetSpeed;
+
+    [SerializeField] Transform shootRate;
+
+    Color colorOrig;
+
+    Vector3 playerDirection;
+
+    float shootTimer;
+
+    bool playerInRange;
+   
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        colorOrig = model.material.color;
+        shootTimer = 0f;
+        // waiting on gamemanager script from Theo
+        //if (gamemanager.instance != null)
+        //{
+            //gamemanager.instance.updateGameGoal(1);
+        //}
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        shootTimer += Time.deltaTime;
+        //waiting on gamemanagerscript
     }
 }
