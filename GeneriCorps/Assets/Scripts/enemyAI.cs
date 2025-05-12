552a224d993a55a8cc76af9da1306b1712cde021
Edisonmatos5 <ejmatosespinal@student.fullsail.edu>
2025-05-11 21:30:31 -0400

    Setting up SerializeFields waiting on teammmates to decide which enemy we will be using.

---
+++ GeneriCorps/Assets/Scripts/enemyAI.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class enemyAI : MonoBehaviour
+{
+    [SerializeField] Renderer model;
+    [SerializeField] int HP;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
