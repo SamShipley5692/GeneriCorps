5c03ce9dd43bdd2a084083817bf43b626f94536c
Giovanni Casillas <TheDemonSpy@Gmail.Com>
2025-05-13 19:06:28 -0700

    Making sure  the commit comments going through

--- GeneriCorps/Assets/Scripts/enemyAI.cs
+++ GeneriCorps/Assets/Scripts/enemyAI.cs
@@ -25,7 +25,7 @@ public class enemyAI : MonoBehaviour //,//IDamage
     float fireCooldown;
 
     bool isPlayerNearby;
-   
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -35,7 +35,7 @@ public class enemyAI : MonoBehaviour //,//IDamage
         // waiting on gamemanager script from Theo
         //if (gamemanager.instance != null)
         //{
-            //gamemanager.instance.updateGameGoal(1);
+        //gamemanager.instance.updateGameGoal(1);
         //}
 
     }
@@ -45,7 +45,7 @@ public class enemyAI : MonoBehaviour //,//IDamage
     {
         fireCooldown += Time.deltaTime;
         //if (!isPlayerNearby || gamemanager.instance == null || gamemanager.instance.player == null)
-            return;
+        return;
 
         //Vector3 targetPos = gamemanager.instance.player.transform.position;
         //lookDirection = targetPos - transform.position;
@@ -54,7 +54,7 @@ public class enemyAI : MonoBehaviour //,//IDamage
 
         //if (navAgent.remainingDistance <= navAgent.stoppingDistance)
         //{
-            //turnToFace();
+        //turnToFace();
         //}
 
         //if (fireCooldown >= fireDelay)
@@ -63,25 +63,25 @@ public class enemyAI : MonoBehaviour //,//IDamage
         }
     }
 
-    //Collider code done. Awaiting gameManager
+    //Gio: Collider code done. Awaiting gameManager
     private void OnTriggerEnter(Collider other)
     {
-       {
-        if (other.CompareTag("Player"))
         {
-            isPlayerNearby = true;
+            if (other.CompareTag("Player"))
+            {
+                isPlayerNearby = true;
+            }
         }
-       }
     }
 
     private void OnTriggerExit(Collider other)
     {
-       void OnTriggerExit(Collider other)
+        void OnTriggerExit(Collider other)
         {
-        if (other.CompareTag("Player"))
-        {
-            isPlayerNearby = false;
-        }
+            if (other.CompareTag("Player"))
+            {
+                isPlayerNearby = false;
+            }
         }
     }
 
@@ -127,7 +127,7 @@ public class enemyAI : MonoBehaviour //,//IDamage
     void fire()
     {
         //Fire cooldown for ranged enemy. Awaiting GameManger
-    { /* if (fireCooldown <= 0f)
+        { /* if (fireCooldown <= 0f)
         {
             fireCooldown = 1f / fireRate;
             if (projectilePrefab != null && firePoint != null)
@@ -144,6 +144,7 @@ public class enemyAI : MonoBehaviour //,//IDamage
                 Debug.LogError("Projectile Prefab or Fire is not assigned");
             }
         } */
+        }
     }
 }
 
