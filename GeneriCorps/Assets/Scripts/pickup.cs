29af1662c43d7534ce1644280e6bc3679f1bb202
Edisonmatos5 <ejmatosespinal@student.fullsail.edu>
2025-05-20 18:55:17 -0400

    Created pickup script for player commented code out for now will return later and update

---
+++ GeneriCorps/Assets/Scripts/pickup.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+//public class pickup : MonoBehaviour
+{
+    //public weaponStats weaponData;
+
+    //private void //OnTriggerEnter(Collider other)
+    {
+        //if (other.CompareTag("Player"))
+        {
+           //IPickup pickupInterface = other.GetComponent<IPickup>();
+            //if (pickupInterface != null)
+            {
+                //pickupInterface.getWeaponStats(weaponData);
+               // Destroy(gameObject);
+            }
+        }
+    }
+}
