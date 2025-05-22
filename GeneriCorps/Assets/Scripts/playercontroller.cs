0353909873f2870298fa6a330e80a343340f2772
tymelja <162160340+tymelja@users.noreply.github.com>
2025-05-21 20:58:34 -0400

    updated the playercontroller have to go back to it after gunstats get updated tomorrow

--- GeneriCorps/Assets/Scripts/playercontroller.cs
+++ GeneriCorps/Assets/Scripts/playercontroller.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 public class playercontroller : MonoBehaviour
 {
     [SerializeField] CharacterController controller;
@@ -15,6 +15,7 @@ public class playercontroller : MonoBehaviour
 
     bool isSprinting;
     int jumpCount;
+    [SerializeField] int HP;
     int HPOrig;
 
     [SerializeField] int speed;
@@ -24,23 +25,29 @@ public class playercontroller : MonoBehaviour
     [SerializeField] int jumpForce;
 
     // Weapon
+    [SerializeField] GameObject gunModel;
     [SerializeField] int shootDamage;
     [SerializeField] float shootRate;
     [SerializeField] int shootDist;
+
+    // Inventory
+    //[SerializeField] List<gunStats> gunList = new List<gunStats>();
+    int gunListPos;
 
     float shootTimer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        HPOrig = HP;
+        UpdatePlayerUI();
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDist, Color.red);
-
+        if(!gameManager.instance.isPaused)
         movement();
         Sprint();
     }
@@ -67,7 +74,7 @@ public class playercontroller : MonoBehaviour
         if (Input.GetButtonDown("Fire1") && shootTimer > shootRate)
             Shoot();
 
-
+        selectGun();
     }
 
     void Sprint()
@@ -116,18 +123,46 @@ public class playercontroller : MonoBehaviour
 
     public void TakeDamage(int amount) 
     {
-        //HP -= amount;
+        HP -= amount;
 
-        // check for death
+         //check for death
 
-        //if (HP <= 0)
-        //{
-        //    gamemanager.instance.youLose();
-        //}
+        if (HP <= 0)
+        {
+            //gameManager.instance.youLose();
+        }
     }
     public void UpdatePlayerUI()
     {
-        //GameManger.instance.playerHPBar.fillAmount = (float)HP / HPOrig;
+        //gameManager.instance.playerHPBar.fillAmount = (float)HP / HPOrig;
     }
-   
+   IEnumerator flashDamageScreen()
+    {
+        //gameManager.instance.playerDMGScreen.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+       // gameManager.instance.playerDMGScreen.SetActive(false);
+    }
+
+    void selectGun()
+    {
+        //if(Input.GetAxis("Mouse ScrollWheel") > 0 && gunListPos < gunListPos.Count - 1)
+        {
+            gunListPos++;
+            changeGun();
+        }
+        // else  if(Input.GetAxis("Mouse ScrollWheel") > 0 && gunListPos > 0){
+        gunListPos--;
+        changeGun();
+    }
+
+    void changeGun()
+    {
+        //shootDamage = gunListPos[gunListPos].shootDamage;
+    }
+
+    //public void getGunStats(gunStats gun) will add changes later;;
+    
+
+
+
 }
