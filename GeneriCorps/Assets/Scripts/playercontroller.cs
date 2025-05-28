29edd5af6b16873a844dc57c1c7c9afaed9f89b3
Gregory Jones <GCJones@student.fullsail.edu>
2025-05-27 20:08:22 -0500

    finished up audio methods in playercontroller. added basic sounds to player

--- GeneriCorps/Assets/Scripts/playercontroller.cs
+++ GeneriCorps/Assets/Scripts/playercontroller.cs
@@ -76,9 +76,9 @@ public class playercontroller : MonoBehaviour, IDamage, IPickup, IOpen
 
         if (controller.isGrounded)
         {
-            if(moveDir.normalized.magnitude > 0.3f && !isPlayingStep){
+            if(moveDir.normalized.magnitude > 0.3f && !isPlayingStep)
                 StartCoroutine(PlayStep());
-            }
+            
             jumpCount = 0;
             playerVel = Vector3.zero;
         }
@@ -95,7 +95,7 @@ public class playercontroller : MonoBehaviour, IDamage, IPickup, IOpen
         
         playerVel.y -= gravity * Time.deltaTime;
 
-        if (Input.GetButtonDown("Fire1") && shootTimer > shootRate)
+        if (Input.GetButtonDown("Fire1") && weaponInv.Count > 0 && shootTimer > shootRate)
         {
             Shoot();
         }
@@ -145,10 +145,13 @@ public class playercontroller : MonoBehaviour, IDamage, IPickup, IOpen
     {
         shootTimer = 0;
 
+        aud.PlayOneShot(weaponInv[weaponInvPos].shootSound[Random.Range(0, weaponInv[weaponInvPos].shootSound.Length)], weaponInv[weaponInvPos].shootSoundVol);
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDist, ~ignoreLayer))
         {
-            Debug.Log(hit.transform.name);
+            //Debug.Log(hit.transform.name);
+
+            Instantiate(weaponInv[weaponInvPos].hitEffect, hit.point, Quaternion.identity);
 
             IDamage dmg = hit.collider.GetComponent<IDamage>();
 
@@ -161,6 +164,7 @@ public class playercontroller : MonoBehaviour, IDamage, IPickup, IOpen
 
     public void takeDamage(int amount) 
     {
+        aud.PlayOneShot(audHurt[Random.Range(0, audHurt.Length)], audHurtVol);
         hp -= amount;
         updatePlayerUI();
         StartCoroutine(flashDamageScreen());
