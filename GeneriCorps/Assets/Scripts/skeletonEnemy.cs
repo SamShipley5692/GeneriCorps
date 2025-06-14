74c4902ad9d1a0e022866df9cfdf4b6f902349b1
Samantha Shipley <SMShipley@student.fullsail.edu>
2025-06-14 08:26:57 -0500

    Added code to use audio on skeleton warrior.

--- GeneriCorps/Assets/Scripts/skeletonEnemy.cs
+++ GeneriCorps/Assets/Scripts/skeletonEnemy.cs
@@ -21,6 +21,17 @@ public class skeletonEnemy : MonoBehaviour, IDamage
     [SerializeField][Range(0.1f, 2)] float attackRate;
     [SerializeField][Range(0.1f, 15)] float deathScaleDuration;
 
+    [SerializeField] AudioSource effectAudio;
+    [SerializeField] AudioClip[] audDeath;
+    [Range(0, 1)][SerializeField] float audDeathVol;
+    [SerializeField] AudioClip[] audHurt;
+    [Range(0, 1)][SerializeField] float audHurtVol;
+    [SerializeField] AudioClip[] audWalk;
+    [Range(0, 1)][SerializeField] float audWalkVol;
+    [SerializeField] AudioClip[] audAttack;
+    [Range(0, 1)][SerializeField] float audAttackVol;
+
+
     Vector3 playerDir;
     Vector3 startingPos;
     Vector3 startScale;
@@ -31,6 +42,7 @@ public class skeletonEnemy : MonoBehaviour, IDamage
     float stoppingDistOrig;
 
     bool playerInRange;
+    bool isPlayingStep;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -92,12 +104,28 @@ public class skeletonEnemy : MonoBehaviour, IDamage
         roamTimer = 0;
         agent.stoppingDistance = 0;
 
+        if (effectAudio != null && audWalk.Length > 0)
+        {
+            if (!isPlayingStep)
+                StartCoroutine(PlayStep());
+        }
+
         Vector3 ranPos = Random.insideUnitSphere * roamDist;
         ranPos += startingPos;
 
         NavMeshHit hit;
         NavMesh.SamplePosition(ranPos, out hit, roamDist, 1);
         agent.SetDestination(hit.position);
+    }
+
+    IEnumerator PlayStep()
+    {
+        isPlayingStep = true; // isPlayingFlight
+        effectAudio.PlayOneShot(audWalk[Random.Range(0, audWalk.Length)], audWalkVol);
+
+        yield return new WaitForSeconds(0.4f);
+
+        isPlayingStep = false;
     }
 
     bool canSeePlayer()
@@ -151,12 +179,18 @@ public class skeletonEnemy : MonoBehaviour, IDamage
 
     public void takeDamage(int amount)
     {
+        
+
         HP -= amount;
 
         agent.SetDestination(gameManager.instance.player.transform.position);
 
         if (HP <= 0)
         {
+            if (effectAudio != null && audDeath.Length > 0)
+            {
+                effectAudio.PlayOneShot(audDeath[Random.Range(0, audDeath.Length)], audDeathVol);
+            }
             gameManager.instance.updateGameGoal(-1);
             playerInRange = false;
             anim.SetTrigger("die");
@@ -168,6 +202,10 @@ public class skeletonEnemy : MonoBehaviour, IDamage
         }
         else
         {
+            if (effectAudio != null && audHurt.Length > 0)
+            {
+                effectAudio.PlayOneShot(audHurt[Random.Range(0, audHurt.Length)], audHurtVol);
+            }
             anim.SetTrigger("damage");
         }
     }
@@ -194,6 +232,11 @@ public class skeletonEnemy : MonoBehaviour, IDamage
 
     public void attack()
     {
+        if (effectAudio != null && audAttack.Length > 0)
+        {
+            effectAudio.PlayOneShot(audAttack[Random.Range(0, audAttack.Length)], audAttackVol);
+        }
+
         anim.SetTrigger("attack");
         attackTimer = 0;
     }
