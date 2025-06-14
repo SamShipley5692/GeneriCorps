7ef5703e9d0952584ffd053ff384ab74b0d76dba
tymelja <162160340+tymelja@users.noreply.github.com>
2025-06-13 23:01:31 -0400

    PlayerHealth script

---
+++ GeneriCorps/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int health;
+    public int maxHealth = 10;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if(health < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
