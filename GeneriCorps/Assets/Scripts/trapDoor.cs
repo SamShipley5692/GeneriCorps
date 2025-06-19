4b41f72f6fd95663ee0de216663d0fc256e972f7
Samantha Shipley <SMShipley@student.fullsail.edu>
2025-06-19 17:04:00 -0500

    Improved trap door script.

--- GeneriCorps/Assets/Scripts/trapDoor.cs
+++ GeneriCorps/Assets/Scripts/trapDoor.cs
@@ -3,8 +3,20 @@ using UnityEngine;
 public class trapDoor : MonoBehaviour
 {
     [SerializeField] GameObject trapDoorPrefab;
+    Animator hingeAnim;
+
+    private void Start()
+    {
+        hingeAnim = trapDoorPrefab.GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        trapDoorPrefab.GetComponent<Animation>().Play("TrapDoorAnim");
+        //trapDoorPrefab.GetComponent<Animation>().Play("TrapDoorAnim");
+        hingeAnim.SetTrigger("open");
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        hingeAnim.SetTrigger("close");
     }
 }
