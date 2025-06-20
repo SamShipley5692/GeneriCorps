f00603dd61ccf588af826b5832194c7334256b58
Edisonmatos5 <ejmatosespinal@student.fullsail.edu>
2025-06-20 03:54:58 -0400

    Camera Shake Script completed, Made significant work and progress on EnemyHealthUI, CameraBobbing, Floating Damage Text, and updated enemy Ai script as well

---
+++ GeneriCorps/Assets/Scripts/FloatingDamageText.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using TMPro;
+
+public class FloatingDamageText : MonoBehaviour
+{
+    [SerializeField] float riseSpeed = 1f;
+    [SerializeField] float lifetime = 3f;
+    private TextMeshPro text;
+    private Transform cam;
+
+    void Start()
+    {
+        cam = Camera.main.transform;
+        text = GetComponent<TextMeshPro>();
+        Destroy(gameObject, lifetime);
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.up * riseSpeed * Time.deltaTime;
+        transform.LookAt(cam);
+        transform.Rotate(0, 180f, 0); 
+    }
+
+    public void SetDamage(int damage)
+    {
+        if (text != null)
+            text.text = damage.ToString();
+    }
+}
