5e121b30bd9653e26ad25896346cdffdf81e3291
Tedric <SirTedric@outlook.com>
2025-06-22 03:15:54 -0400

    Credit menu
    credits adjusted

--- GeneriCorps/Assets/Scripts/Credits.cs
+++ GeneriCorps/Assets/Scripts/Credits.cs
@@ -1,9 +1,15 @@
+using System.Collections;
+using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Credits : MonoBehaviour
 {
     public float scrollSpeed;
+    public string levelName;
+
+    private float deadZone = 775;   // setting the Y axis transform location to set back to main menu
 
     private RectTransform rectTransform;
 
@@ -16,6 +22,8 @@ public class Credits : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ShowMainMenuAfterCredits();
+
         if (Input.GetButton("SpeedUp Credits"))
         {
             rectTransform.anchoredPosition += new Vector2(0, (scrollSpeed * Time.deltaTime) * 10);
@@ -24,5 +32,11 @@ public class Credits : MonoBehaviour
         {
             rectTransform.anchoredPosition += new Vector2(0, scrollSpeed * Time.deltaTime);
         }
+    }
+
+    private IEnumerator ShowMainMenuAfterCredits()
+    {
+        yield return new WaitForSecondsRealtime(70);
+        SceneManager.LoadScene(levelName);
     }
 }
