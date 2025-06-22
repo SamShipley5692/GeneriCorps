e9208e2ba68c080b9d40cc0062bf18c02f613559
tymelja <162160340+tymelja@users.noreply.github.com>
2025-06-21 22:49:27 -0400

    added sound to staff

--- GeneriCorps/Assets/Scripts/Staff.cs
+++ GeneriCorps/Assets/Scripts/Staff.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Staff", menuName = "Scriptable Objects/Staff", order =1)]
@@ -10,5 +11,13 @@ public class Staff : ScriptableObject
     
 
     public Vector3[] spawnPoints;
+    AudioSource m_shootingSound;
 
+
+    void Start() => m_shootingSound = GetCompenent<AudioSource>();
+
+    private T GetCompenent<T>()
+    {
+        throw new NotImplementedException();
+    }
 }
