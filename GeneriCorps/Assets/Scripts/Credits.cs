d0b446995b4103917f767fba4a1097388b102f85
Edisonmatos5 <ejmatosespinal@student.fullsail.edu>
2025-06-28 02:52:08 -0400

    fixed credits freezing

--- GeneriCorps/Assets/Scripts/Credits.cs
+++ GeneriCorps/Assets/Scripts/Credits.cs
@@ -15,6 +15,7 @@ public class Credits : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Time.timeScale = 1f;
         rectTransform = GetComponent<RectTransform>();
     }
 
